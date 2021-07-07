5e3c2fd4cf21d875c3330e28589ed91a66884452
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-07 14:56:30 +0530

    inital

--- consumer-service/src/main/java/com/pkglobal/subscriber/model/Address.java
+++ consumer-service/src/main/java/com/pkglobal/subscriber/model/Address.java
@@ -1,95 +1,93 @@
 package com.pkglobal.subscriber.model;
 
-
 public class Address {
-    private String addressLine1;
+	private String addressLine1;
 
-    private String addressLine2;
+	private String addressLine2;
 
-    private String street;
+	private String street;
 
-    private String postalCode;
+	private String postalCode;
 
-    public String getAddressLine1() {
-        return addressLine1;
-    }
+	public String getAddressLine1() {
+		return addressLine1;
+	}
 
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
 
-    public String getAddressLine2() {
-        return addressLine2;
-    }
+	public String getAddressLine2() {
+		return addressLine2;
+	}
 
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
 
-    public String getStreet() {
-        return street;
-    }
+	public String getStreet() {
+		return street;
+	}
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
+	public void setStreet(String street) {
+		this.street = street;
+	}
 
-    public String getPostalCode() {
-        return postalCode;
-    }
+	public String getPostalCode() {
+		return postalCode;
+	}
 
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 
-    @Override
-    public String toString() {
-        return "Address [addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2
-                + ", street=" + street + ", postalCode=" + postalCode + "]";
-    }
+	@Override
+	public String toString() {
+		return "Address [addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2 + ", street=" + street
+				+ ", postalCode=" + postalCode + "]";
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((addressLine1 == null) ? 0 : addressLine1.hashCode());
-        result = prime * result + ((addressLine2 == null) ? 0 : addressLine2.hashCode());
-        result = prime * result + ((postalCode == null) ? 0 : postalCode.hashCode());
-        result = prime * result + ((street == null) ? 0 : street.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addressLine1 == null) ? 0 : addressLine1.hashCode());
+		result = prime * result + ((addressLine2 == null) ? 0 : addressLine2.hashCode());
+		result = prime * result + ((postalCode == null) ? 0 : postalCode.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Address other = (Address) obj;
-        if (addressLine1 == null) {
-            if (other.addressLine1 != null)
-                return false;
-        } else if (!addressLine1.equals(other.addressLine1))
-            return false;
-        if (addressLine2 == null) {
-            if (other.addressLine2 != null)
-                return false;
-        } else if (!addressLine2.equals(other.addressLine2))
-            return false;
-        if (postalCode == null) {
-            if (other.postalCode != null)
-                return false;
-        } else if (!postalCode.equals(other.postalCode))
-            return false;
-        if (street == null) {
-            if (other.street != null)
-                return false;
-        } else if (!street.equals(other.street))
-            return false;
-        return true;
-    }
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (addressLine1 == null) {
+			if (other.addressLine1 != null)
+				return false;
+		} else if (!addressLine1.equals(other.addressLine1))
+			return false;
+		if (addressLine2 == null) {
+			if (other.addressLine2 != null)
+				return false;
+		} else if (!addressLine2.equals(other.addressLine2))
+			return false;
+		if (postalCode == null) {
+			if (other.postalCode != null)
+				return false;
+		} else if (!postalCode.equals(other.postalCode))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
 
 }
--- consumer-service/src/main/java/com/pkglobal/subscriber/model/MessageRequest.java
+++ consumer-service/src/main/java/com/pkglobal/subscriber/model/MessageRequest.java
@@ -3,189 +3,188 @@ package com.pkglobal.subscriber.model;
 import com.pkglobal.subscriber.util.CustomerStatusEnum;
 
 public class MessageRequest {
-    private String customerNumber;
+	private String customerNumber;
 
-    private String firstName;
+	private String firstName;
 
-    private String lastName;
+	private String lastName;
 
-    private String birthDate;
+	private String birthDate;
 
-    private String country;
+	private String country;
 
-    private String countryCode;
+	private String countryCode;
 
-    private String mobileNumber;
+	private String mobileNumber;
 
-    private String email;
+	private String email;
 
-    private CustomerStatusEnum customerStatus;
+	private CustomerStatusEnum customerStatus;
 
-    private Address address;
+	private Address address;
 
-    public String getCustomerNumber() {
-        return customerNumber;
-    }
+	public String getCustomerNumber() {
+		return customerNumber;
+	}
 
-    public void setCustomerNumber(String customerNumber) {
-        this.customerNumber = customerNumber;
-    }
+	public void setCustomerNumber(String customerNumber) {
+		this.customerNumber = customerNumber;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getBirthDate() {
-        return birthDate;
-    }
+	public String getBirthDate() {
+		return birthDate;
+	}
 
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
 
-    public String getCountry() {
-        return country;
-    }
+	public String getCountry() {
+		return country;
+	}
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-    public String getCountryCode() {
-        return countryCode;
-    }
+	public String getCountryCode() {
+		return countryCode;
+	}
 
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
 
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
 
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public CustomerStatusEnum getCustomerStatus() {
-        return customerStatus;
-    }
+	public CustomerStatusEnum getCustomerStatus() {
+		return customerStatus;
+	}
 
-    public void setCustomerStatus(CustomerStatusEnum customerStatus) {
-        this.customerStatus = customerStatus;
-    }
+	public void setCustomerStatus(CustomerStatusEnum customerStatus) {
+		this.customerStatus = customerStatus;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
+	public Address getAddress() {
+		return address;
+	}
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
-        result = prime * result + ((country == null) ? 0 : country.hashCode());
-        result = prime * result + ((countryCode == null) ? 0 : countryCode.hashCode());
-        result = prime * result + ((customerNumber == null) ? 0 : customerNumber.hashCode());
-        result = prime * result + ((customerStatus == null) ? 0 : customerStatus.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        result = prime * result + ((mobileNumber == null) ? 0 : mobileNumber.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((countryCode == null) ? 0 : countryCode.hashCode());
+		result = prime * result + ((customerNumber == null) ? 0 : customerNumber.hashCode());
+		result = prime * result + ((customerStatus == null) ? 0 : customerStatus.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((mobileNumber == null) ? 0 : mobileNumber.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        MessageRequest other = (MessageRequest) obj;
-        if (address == null) {
-            if (other.address != null)
-                return false;
-        } else if (!address.equals(other.address))
-            return false;
-        if (birthDate == null) {
-            if (other.birthDate != null)
-                return false;
-        } else if (!birthDate.equals(other.birthDate))
-            return false;
-        if (country == null) {
-            if (other.country != null)
-                return false;
-        } else if (!country.equals(other.country))
-            return false;
-        if (countryCode == null) {
-            if (other.countryCode != null)
-                return false;
-        } else if (!countryCode.equals(other.countryCode))
-            return false;
-        if (customerNumber == null) {
-            if (other.customerNumber != null)
-                return false;
-        } else if (!customerNumber.equals(other.customerNumber))
-            return false;
-        if (customerStatus != other.customerStatus)
-            return false;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
-        if (firstName == null) {
-            if (other.firstName != null)
-                return false;
-        } else if (!firstName.equals(other.firstName))
-            return false;
-        if (lastName == null) {
-            if (other.lastName != null)
-                return false;
-        } else if (!lastName.equals(other.lastName))
-            return false;
-        if (mobileNumber == null) {
-            if (other.mobileNumber != null)
-                return false;
-        } else if (!mobileNumber.equals(other.mobileNumber))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MessageRequest other = (MessageRequest) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (birthDate == null) {
+			if (other.birthDate != null)
+				return false;
+		} else if (!birthDate.equals(other.birthDate))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (countryCode == null) {
+			if (other.countryCode != null)
+				return false;
+		} else if (!countryCode.equals(other.countryCode))
+			return false;
+		if (customerNumber == null) {
+			if (other.customerNumber != null)
+				return false;
+		} else if (!customerNumber.equals(other.customerNumber))
+			return false;
+		if (customerStatus != other.customerStatus)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (mobileNumber == null) {
+			if (other.mobileNumber != null)
+				return false;
+		} else if (!mobileNumber.equals(other.mobileNumber))
+			return false;
+		return true;
+	}
 
-    @Override
-    public String toString() {
-        return "MessageRequest [customerNumber=" + customerNumber + ", firstName=" + firstName
-                + ", lastName=" + lastName + ", birthDate=" + birthDate + ", country=" + country
-                + ", countryCode=" + countryCode + ", mobileNumber=" + mobileNumber + ", email="
-                + email + ", customerStatus=" + customerStatus + ", address=" + address + "]";
-    }
-
+	@Override
+	public String toString() {
+		return "MessageRequest [customerNumber=" + customerNumber + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", birthDate=" + birthDate + ", country=" + country + ", countryCode=" + countryCode
+				+ ", mobileNumber=" + mobileNumber + ", email=" + email + ", customerStatus=" + customerStatus
+				+ ", address=" + address + "]";
+	}
 
 }
