7898facfa656f8facd8bbd52f3849fdb9d634a14
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-09 16:51:19 +0530

    commit

--- consumer-service/src/main/java/com/pkglobal/subscriber/service/DefaultConsumerService.java
+++ consumer-service/src/main/java/com/pkglobal/subscriber/service/DefaultConsumerService.java
@@ -1,6 +1,7 @@
 package com.pkglobal.subscriber.service;
 
 import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,45 +18,41 @@ import com.pkglobal.subscriber.util.ObjectMapperUtil;
 @Service
 public class DefaultConsumerService implements ConsumerService {
 
-    private static final Logger logger = LoggerFactory.getLogger(DefaultConsumerService.class);
+	private static final Logger logger = LoggerFactory.getLogger(DefaultConsumerService.class);
 
-    @Autowired
-    private AuditDataRepository auditDataRepository;
+	@Autowired
+	private AuditDataRepository auditDataRepository;
 
-    @Autowired
-    private DefaultMessageRequestMaskConverter messageRequestConverter;
+	@Autowired
+	private DefaultMessageRequestMaskConverter messageRequestConverter;
 
-    @Override
-    @KafkaListener(topics = "${cloudkarafka.topic}")
-    public void consumeService(String messageRequestString) {
-        MessageRequest messageRequest =
-                ObjectMapperUtil.returnObjectFromJsonString(messageRequestString);
-        MessageRequest maskMessageRequest = messageRequestConverter.convert(messageRequest);
-        UUID uuid = UUID.randomUUID();
-        logger.info("Started to consume messageRequest : {} and UUID:{} ", maskMessageRequest,
-                uuid);
-        Audit auditEntity = buildAuditEntity(messageRequestString, messageRequest);
-        auditDataRepository.save(auditEntity);
-        MessageResponse response = buildMessageResponse();
-        logger.info("Finished to consume message : {} and UUID:{} ", response, uuid);
+	@Override
+	@KafkaListener(topics = "${cloudkarafka.topic}")
+	public void consumeService(String messageRequestString) {
+		System.out.println(messageRequestString);
+		MessageRequest messageRequest = ObjectMapperUtil.returnObjectFromJsonString(messageRequestString);
+		MessageRequest maskMessageRequest = messageRequestConverter.convert(messageRequest);
+		UUID uuid = UUID.randomUUID();
+		logger.info("Started to consume messageRequest : {} and UUID:{} ", maskMessageRequest, uuid);
+		Audit auditEntity = buildAuditEntity(messageRequestString, messageRequest);
+		auditDataRepository.save(auditEntity);
+		MessageResponse response = buildMessageResponse();
+		logger.info("Finished to consume message : {} and UUID:{} ", response, uuid);
 
-    }
+	}
 
+	private Audit buildAuditEntity(String messageRequestString, MessageRequest messageRequest) {
+		Audit auditEntity = new Audit();
+		auditEntity.setCustomerNumber(messageRequest.getCustomerNumber());
+		auditEntity.setPayload(messageRequestString);
+		return auditEntity;
+	}
 
-    private Audit buildAuditEntity(String messageRequestString, MessageRequest messageRequest) {
-        Audit auditEntity = new Audit();
-        auditEntity.setCustomerNumber(messageRequest.getCustomerNumber());
-        auditEntity.setPayload(messageRequestString);
-        return auditEntity;
-    }
-
-    private MessageResponse buildMessageResponse() {
-        MessageResponse response = new MessageResponse();
-        response.setData("Consumed Message sucessfully");
-        response.setStatus("success");
-        return response;
-    }
-
-
+	private MessageResponse buildMessageResponse() {
+		MessageResponse response = new MessageResponse();
+		response.setData("Consumed Message sucessfully");
+		response.setStatus("success");
+		return response;
+	}
 
 }
