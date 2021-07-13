50e9338f46566b14dff4f7971a106116478ce992
mpavan <mpavan@mpavan-lp.pkrmsezdc.com>
2021-07-13 11:48:51 +0530

    commit

--- consumer-service/src/main/java/com/pkglobal/subscriber/SpringBootSubscriberApplication.java
+++ consumer-service/src/main/java/com/pkglobal/subscriber/SpringBootSubscriberApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSubscriberApplication {
 
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootSubscriberApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSubscriberApplication.class, args);
+	}
 
 }
--- consumer-service/src/main/java/com/pkglobal/subscriber/aop/DefaultSubscriberAspect.java
+++ consumer-service/src/main/java/com/pkglobal/subscriber/aop/DefaultSubscriberAspect.java
@@ -1,7 +1,9 @@
 package com.pkglobal.subscriber.aop;
 
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,38 +17,50 @@ import com.pkglobal.subscriber.repository.ErrorDataRepository;
 @Component
 public class DefaultSubscriberAspect {
 
-    private static final Logger logger = LoggerFactory.getLogger(DefaultSubscriberAspect.class);
+	private static final Logger logger = LoggerFactory.getLogger(DefaultSubscriberAspect.class);
 
-    @Autowired
-    private ErrorDataRepository errorDataRepository;
+	@Autowired
+	private ErrorDataRepository errorDataRepository;
 
-    @AfterThrowing(
-            pointcut = "execution(* com.pkglobal.subscriber.service.DefaultConsumerService.*(..)) and args(messageRequestString)",
-            throwing = "ex")
-    public void logError(Exception ex, String messageRequestString) {
-        Error errorEntity = buildErrorEntity(messageRequestString, ex);
-        errorDataRepository.save(errorEntity);
-        ErrorResponse errorResponse = buildErrorResponse(ex);
-        logger.error("ErrorResponse :{}", errorResponse);
-    }
+	@AfterThrowing(pointcut = "execution(* com.pkglobal.subscriber.service.DefaultConsumerService.*(..)) and args(messageRequestString)", throwing = "ex")
+	public void logError(Exception ex, String messageRequestString) {
+		Error errorEntity = buildErrorEntity(messageRequestString, ex);
+		errorDataRepository.save(errorEntity);
+		ErrorResponse errorResponse = buildErrorResponse(ex);
+		logger.error("ErrorResponse :{}", errorResponse);
+	}
 
-    private Error buildErrorEntity(String messageRequestString, Exception e) {
-        Error errorEntity = new Error();
-        errorEntity.setPayload(messageRequestString);
-        errorEntity.setErrorDescription(e.getMessage());
-        errorEntity.setErrorType(e.getClass().getName());
-        return errorEntity;
-    }
+	@Before(value = "execution(* com.pkglobal.subscriber.service.DefaultConsumerService.*(..)) and args(messageRequestString)")
+	public void beforeAdvice(Exception ex, String messageRequestString) {
+		Error errorEntity = buildErrorEntity(messageRequestString, ex);
+		errorDataRepository.save(errorEntity);
+		ErrorResponse errorResponse = buildErrorResponse(ex);
+		logger.error("ErrorResponse :{}", errorResponse);
+	}
 
-    private ErrorResponse buildErrorResponse(Exception e) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus("error");
-        errorResponse.setMessage(e.getMessage());
-        errorResponse.setErrorType(e.getClass().getName());
-        logger.error("ErrorResponse :{}", errorResponse);
-        return errorResponse;
-    }
+	@Around(value = "execution(* com.pkglobal.subscriber.service.DefaultConsumerService.*(..)) and args(messageRequestString)")
+	public void afterAdvice(Exception ex, String messageRequestString) {
+		Error errorEntity = buildErrorEntity(messageRequestString, ex);
+		errorDataRepository.save(errorEntity);
+		ErrorResponse errorResponse = buildErrorResponse(ex);
+		logger.error("ErrorResponse :{}", errorResponse);
+	}
+
+	private Error buildErrorEntity(String messageRequestString, Exception e) {
+		Error errorEntity = new Error();
+		errorEntity.setPayload(messageRequestString);
+		errorEntity.setErrorDescription(e.getMessage());
+		errorEntity.setErrorType(e.getClass().getName());
+		return errorEntity;
+	}
+
+	private ErrorResponse buildErrorResponse(Exception e) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setStatus("error");
+		errorResponse.setMessage(e.getMessage());
+		errorResponse.setErrorType(e.getClass().getName());
+		logger.error("ErrorResponse :{}", errorResponse);
+		return errorResponse;
+	}
 
 }
-
-
